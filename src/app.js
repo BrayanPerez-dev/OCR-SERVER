6376529d14cd76c13de10949d0efd87941fdc1f4
Brayan-Perez-Martinez <pbryantsam@gmail.com>
2022-01-10 10:41:27 -0600

    changed modules

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
-import documentRoute from './routes/identityDocument.route';
+import documentRoute from './routes/identityDocument.route.js';
 import dotenv from 'dotenv';
 
 dotenv.config()
