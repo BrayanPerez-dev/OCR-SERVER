c75e19196166962183f943036519a022314f6448
Brayan-Perez-Martinez <pbryantsam@gmail.com>
2022-01-12 08:31:35 -0600

    added body-parser to middlewares

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
+import bodyParser from 'body-parser';
 import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
 import documentRoute from './routes/identityDocument.route.js';
 import dotenv from 'dotenv';
-
 dotenv.config()
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
 
 const app = express();
 
@@ -12,8 +16,6 @@ app.set('port', process.env.PORT || 4000)
 app.set('json spaces', 4);
 app.use(cors());
 app.use(helmet());
-app.use(express.json());
-app.use(express.urlencoded({extended:false}));
 app.get('/',(req,res)=>{
     res.json({
         message: "Welcome to the server scanner intellityc",
