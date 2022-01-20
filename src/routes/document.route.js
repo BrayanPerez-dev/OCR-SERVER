4ed97d8e2dde658f9462d0355de5446aa2abe57e
Brayan-Perez-Martinez <pbryantsam@gmail.com>
2022-01-20 13:46:22 -0600

    removed middlewares from document route

--- src/routes/document.route.js
+++ src/routes/document.route.js
@@ -3,7 +3,7 @@ import { createDocument,getDocuments } from "../controllers/documents.controller
 import {verifyToken} from "../middlewares/authJwt";
 const router = Router();
 
-router.post('/',verifyToken,createDocument)
-router.get('/',verifyToken,getDocuments)
+router.post('/',createDocument)
+router.get('/',getDocuments)
 
 export default router
